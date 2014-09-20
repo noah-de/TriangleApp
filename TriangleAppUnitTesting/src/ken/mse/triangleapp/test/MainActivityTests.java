18eb5a76e7679d31b1c13c07f3a771425378e222
me <me@e0x-mini.WORKGROUP>
2014-09-19 21:38:01 -0700

    Added MainActivityTests class for MainActivity unit tests

---
+++ TriangleAppUnitTesting/src/ken/mse/triangleapp/test/MainActivityTests.java
@@ -0,0 +1,20 @@
+package ken.mse.triangleapp.test;
+
+import ken.mse.triangleapp.MainActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class MainActivityTests extends ActivityInstrumentationTestCase2<MainActivity>{
+	public MainActivityTests(){
+		super(MainActivity.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+}
