8dad7ab46be684dcfaf996c2a8b4ea880d8c8b0b
trotterkt <trotterkt@verizon.net>
2014-09-22 15:36:23 -0700

    remove unused imports.

--- TriangleApp/src/ken/mse/triangleapp/MainActivity.java
+++ TriangleApp/src/ken/mse/triangleapp/MainActivity.java
@@ -1,13 +1,11 @@
 package ken.mse.triangleapp;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
-import android.content.Intent;
 
 public class MainActivity extends Activity {
 	//Activity's Tag for logs
